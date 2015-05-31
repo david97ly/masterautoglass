6a162b035c8b7e4332cab9a88e0242c29ef665dc
David Cortez <david97ly@hotmail.com>
2015-05-31 01:09:44 -0600

    cuerto commit

--- glass/views.py
+++ glass/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render_to_response,redirect
 
 def home(request):
     hola="here"
-    nombrep="Vivimos para servirte"
+    nombrep="Esta es otra prueba"
     compania = "Master auto Glass"
     c = {'hola':hola,'nombre':nombrep,'compa':compania,}
     return render_to_response('home.html',c)
