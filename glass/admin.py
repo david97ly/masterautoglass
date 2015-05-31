574c42953c476e41d75189a35707617cd5250fac
David Cortez <david97ly@hotmail.com>
2015-05-31 15:46:53 -0600

    agregando los modelos

--- glass/admin.py
+++ glass/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from models import *
 
 # Register your models here.
+
+admin.site.register(Fotos)
+admin.site.register(Slide)
+admin.site.register(Mensajeb)
+admin.site.register(Info)
+admin.site.register(Contactos)
+admin.site.register(Servicios)
