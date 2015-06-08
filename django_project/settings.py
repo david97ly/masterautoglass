c8310decf14a09dadabcd0202393d006d12c2795
David Cortez <david97ly@hotmail.com>
2015-06-08 11:37:03 -0600

    cambiando el orden

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'glass',
     'south',
+    'glass',
 )
 
 MIDDLEWARE_CLASSES = (
